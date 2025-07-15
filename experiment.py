959397b01ce2403b3295122303c346e9ca00728e
TristanGchlt <tristangchlt@gmail.com>
2025-07-15 11:56:24 +0200

    tranlation of comments

--- experiment.py
+++ experiment.py
@@ -5,47 +5,45 @@ import importlib.util
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 
-####
-
-# Récupérer les paramètres
+#### PARAMETERES LOADING ####
 
 with open("experiments/input.json", "r", encoding="utf-8") as f:
     input_param = json.load(f)
 
 EXPERIMENT_NAME = input_param["experiment_name"]
-
 # Data
 DATA_SOURCE = input_param["data_source"]["dataset"]
 DATA_PREPROCESSING = input_param["data_source"]["preprocessing"]
-SPLIT_TARGET = input_param["split_param"]["target"] # Variable cible, qui constituera le Y
-SPLIT_PROTECTED = input_param["split_param"]["protected"] # Variable protégée (fairness)
-SPLIT_TEST_SIZE = input_param["split_param"]["test_size"] # Proportion de l'échantillon de test
-SPLIT_SEED = input_param["split_param"]["seed"] # Graine aléatoire pour la réalisation du split
-
+SPLIT_TARGET = input_param["split_param"]["target"]
+SPLIT_PROTECTED = input_param["split_param"]["protected"] 
+SPLIT_TEST_SIZE = input_param["split_param"]["test_size"] 
+SPLIT_SEED = input_param["split_param"]["seed"] 
 # Model
-MODEL_TYPE = input_param["model"]["type"] # Référence à un fichier de modèle pour l'entrainement
-MODEL_HYPERPARAMETERS = input_param["model"]["hyperparameters"] # Les hyperparamètres pour l'entrainement du modèle
-
+MODEL_TYPE = input_param["model"]["type"] 
+MODEL_HYPERPARAMETERS = input_param["model"]["hyperparameters"] 
 # Metrics
-
 PERFORMANCE_METRICS = input_param["performance_metrics"]
 FAIRNESS_METRICS = input_param["fairness_metrics"]
-
-# Corrections
-
+# Other treatments
 FAIRNESS_TREATMENT = input_param["fairness_treatment"]
 
-####
+###############################
 
-# Création du dossier de l'experiment.
+
+
+#### CREATION OF THE EXPERIMENT FOLDER ####
 experiment_path = f"experiments/{EXPERIMENT_NAME}"
 #if not os.path.exists(experiment_path) :
 os.makedirs(experiment_path)
 
-# Lecture du jeu de données
+##############################################
+
+
+
+#### DATA LOADING AND PROCESSING ####
+
 dataset = pd.read_csv(f"data/{DATA_SOURCE}/data.csv")
 
-# Prétraitement des données
 preprocessing_path = Path(f"data/{DATA_SOURCE}/preprocessing/{DATA_PREPROCESSING}")
 spec = importlib.util.spec_from_file_location("preprocessing", preprocessing_path)
 module = importlib.util.module_from_spec(spec)
@@ -53,7 +51,6 @@ spec.loader.exec_module(module)
 preprocess = module.preprocess
 preprocessed_data = preprocess(dataset, experiment_path)
 
-# Split des données
 train, test = train_test_split(preprocessed_data, test_size=SPLIT_TEST_SIZE, random_state=SPLIT_SEED, shuffle=True)
 X_train = train.drop(SPLIT_TARGET, axis=1)
 y_train = train[SPLIT_TARGET]
@@ -74,8 +71,11 @@ y_test.to_csv(f"{experiment_path}/split/y_test.csv", index=None)
 p_train.to_csv(f"{experiment_path}/split/p_train.csv", index=None)
 p_test.to_csv(f"{experiment_path}/split/p_test.csv", index=None)
 
+########################################
 
-# Entrainement du modèle
+
+
+#### MODEL TRAINING AND PREDICTIONS ####
 if MODEL_TYPE == "RandomForestClassifier":
     from sklearn.ensemble import RandomForestClassifier
     import pickle
@@ -84,11 +84,15 @@ if MODEL_TYPE == "RandomForestClassifier":
     with open(f"{experiment_path}/model.pkl", "wb") as f:
         pickle.dump(model, f)
 
-# Prédictions
 y_pred = model.predict(X_test)
 pd.Series(y_pred).to_csv(f"{experiment_path}/y_pred.csv", index=None)
 
-# Mesures
+###########################################
+
+
+
+#### MEASURES ####
+
 input_param["performance_measurments"] = {}
 for metric in PERFORMANCE_METRICS :
     metric_path = Path(f"metrics/performance/{metric}")
@@ -109,7 +113,13 @@ for metric in FAIRNESS_METRICS :
     measure = metric_function(y_test, y_pred, p_test)
     input_param["fairness_measurments"][metric[:-3]] = measure
 
+#################
 
+
+
+#### SAVING PARAMETERS AND MEASURES ####
 
 with open(f"{experiment_path}/parameters.json", "w", encoding="utf-8") as f:
     json.dump(input_param, f, indent=4, ensure_ascii=False)
+
+#######################################
