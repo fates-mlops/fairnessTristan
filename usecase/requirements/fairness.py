90d1ccb5492badd1e41c9cc0574b1144f8dfb4f6
Tristan Gouaichault <tristangchlt@gmail.com>
2025-10-24 14:36:44 +0200

    fix function names in the python checks

--- usecase/requirements/fairness.py
+++ usecase/requirements/fairness.py
@@ -72,9 +72,9 @@ def test_data_set_available() -> bool :
 
 
 # Evidence "measurement" in "fairClassification"
-def metric_measurement_available() -> bool :
+def the_metric_measurement_available() -> bool :
     global FAIRNESS_MEASURE_FUNCTION
-
+    
     if (BASE_DIR / "requirements" / "fairness.json").exists():
         with open(BASE_DIR / "requirements" / "fairness.json", "r") as f:
             req = json.load(f)
